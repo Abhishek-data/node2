9e4093dfaadfcd6a7be320ab331d55e18cb57557
Abhishek-data <abhishekkr1915@gmail.com>
2023-04-09 17:56:29 +0530

    basic nodejs progarmme

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
+const path = require('path')
 const http = require("http");
 
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  console.log("in the middleware2");
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>add Product</button></form>"
-  );
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);  
-  res.redirect('/')
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  console.log("in the middleware2");
-  res.send("<h1>Hello from saharsa</h1>");
-});
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
 
 app.listen(3000);
